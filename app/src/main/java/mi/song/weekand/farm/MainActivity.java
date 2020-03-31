1f2aee2e4dcd9554c6dd3efe52af8e1bbb99dd8c
songmilee <lsm941010@gmail.com>
2020-03-31 21:08:07 +0900

    first init

---
+++ app/src/main/java/mi/song/weekand/farm/MainActivity.java
@@ -0,0 +1,14 @@
+package mi.song.weekand.farm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
